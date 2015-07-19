5d6f5a0a8df7ce711d07828a034b7ea8c0fcc9be
haris@standout.se <haris@standout.se>
2015-07-19 12:37:42 +0200

    Init

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly IList<Comment> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<Comment>
+            {
+                new Comment
+                {
+                    Author = "Daniel Lo Nigro",
+                    Text = "Hello ReactJS.NET World!"
+                },
+                new Comment
+                {
+                    Author = "Pete Hunt",
+                    Text = "This is one comment"
+                },
+                new Comment
+                {
+                    Author = "Jordan Walke",
+                    Text = "This is *another* comment"
+                },
+            };
+        }
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Comments()
+        {
+            return Json(_comments, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult AddComment(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                _comments.Add(comment);
+                return Content("Success");
+            }
+            return Content("Error");
+        }
+    }
+}
---
+++ Web/Scripts/Comment/CommetBox.jsx
@@ -0,0 +1,50 @@
+﻿var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: []};
+  },
+  loadCommentsFromServer: function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', this.props.url, true);
+    xhr.onload = function() {
+      var data = JSON.parse(xhr.responseText);
+      this.setState({ data: data });
+    }.bind(this);
+    xhr.send();
+  },
+  handleCommentSubmit: function(comment) {
+    var comments = this.state.data;
+    var newComments = comments.concat([comment]);
+    this.setState({data: newComments});
+
+    var data = new FormData();
+    data.append('Author', comment.Author);
+    data.append('Text', comment.Text);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', this.props.submitUrl, true);
+
+    xhr.onload = function() {
+      this.loadCommentsFromServer();
+    }.bind(this);
+
+    xhr.send(data);
+  },
+  componentDidMount: function(){
+    this.loadCommentsFromServer();
+    window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
+      </div>
+      );
+  }
+});
+
+React.render(
+  <CommentBox url="/home/comments" submitUrl="/home/addcomment" pollInterval={2000} />,  
+  document.getElementById('content')
+);
